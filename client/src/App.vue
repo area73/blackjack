5912ba02782e888f6668fe5ba4d0ddda42d870fc
rerades <rerades@siete3.com>
2023-12-28 22:55:28 +0100

    adding cards

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,47 @@
 <script setup lang="ts">
 import BJControls from './components/BJControls.vue';
 import BJGameMat from './components/BJGameMat.vue';
+import BJHand from './components/BJHand.vue';
 import BJMarquee from './components/BJMarquee.vue';
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <BJMarquee msg="You did it!" />
+      <h1 class="title ">Black Jack</h1>
     </div>
   </header>
-  <main>
-    <BJGameMat msg="mate" />
-    <BJControls />
+  <main class="playingCards faceImages rotateHand">
+    <BJGameMat msg="mate">
+      <template #news>
+        <BJMarquee msg="You did it!" />
+      </template>
+      <template #controls>
+        <BJControls />
+      </template>
+      <template #dealer>
+        <BJHand :score="23" :cards="['*', 'D-9', 'H-K', 'C-K', 'D-K', 'S-K']" />
+      </template>
+      <template #player>
+        <BJHand :score="19" :cards="['S-J', 'C-5', 'H-Q']" />
+      </template>
+    </BJGameMat>
   </main>
 </template>
 
 <style lang="scss" scoped>
+.title {
+  font-size: 6rem;
+  font-family: 'Carattere',
+    sans-serif;
+  margin: 0.3em 0 0 0;
+  padding: 0;
+  text-align: center;
+  line-height: 1;
+  text-shadow: 0px 1px 9px rgba(1, 6, 20, 0.61);
+
+  &::first-letter {
+    font-size: 8rem;
+  }
+}
 </style>
--- client/src/components/BJGameMat.vue
+++ client/src/components/BJGameMat.vue
@@ -4,10 +4,21 @@ defineProps<{ msg: String }>()
 
 <template>
   <div class="bj-game-mat">
-    Game Mat {{ msg }}
+    <div class="bj-game-mat__news"><slot name="news" /></div>
+    <div class="bj-game-mat__dealer"><slot name="dealer" /></div>
+    <div class="bj-game-mat__player"><slot name="player"/></div>
+    <div class="bj-game-mat__controls"><slot name="controls"/></div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.bj-game-mat {}
+.bj-game-mat {
+  * {
+    border:1px solid #ccc;
+  }
+  &__news {}
+  &__dealer {}
+  &__player {}
+  &__controls {}
+}
 </style>
