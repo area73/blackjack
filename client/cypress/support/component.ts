7eb2f3e56bbf37de65ff26e889f1e9ba3ee7edee
Rodrigo <rerades@siete3.com>
2023-12-30 11:51:21 +0100

    Front component test (#12)
    * refactoring
    
    * adding env files
    
    * adding component test and visual regression
    
    * adding doc and gitflow for front end

---
+++ client/cypress/support/component.ts
@@ -0,0 +1,54 @@
+// ***********************************************************
+// This example support/component.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+import { mount } from 'cypress/vue';
+import { createPinia } from 'pinia';
+import '../../src/baseStyles'; // global CSS styles
+import './commands';
+
+import type { App } from 'vue';
+
+type Options = {
+  global?: {
+    plugins?: any[]
+    components?: any
+  }
+  router?: any
+}
+
+Cypress.Commands.add('mount', (component, options: Options = {}) => {
+  // Setup options object
+  options.global = options.global || {}
+  // ------------------------------
+  // plugins
+  // ------------------------------
+  options.global.plugins = options.global.plugins || []
+  options.global.plugins.push(createPinia())
+  // Add router plugin
+  options.global.plugins.push({
+    install(app: App<Element>) {
+      app.use(options.router)
+    }
+  })
+  // ------------------------------
+  // Components
+  // ------------------------------
+  options.global.components = options.global.components || {}
+  return mount(component, options).then(({ wrapper }) => {
+    return cy.wrap(wrapper).as('vue')
+  })
+})
+
+Cypress.Commands.add('vue', () => cy.wrap(Cypress.vueWrapper) as any)
